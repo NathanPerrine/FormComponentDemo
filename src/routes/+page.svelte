1ba63767cc93d1d74704274b1674f4f3fc5e0fc0
Nathan <nathan.perrine@gmail.com>
2023-08-14 02:22:33 -0400

    form, submit, typing, reset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,101 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    function isFormValid(data: {[fieldName: string]: any}): boolean {
+        if(!isRequiredFieldValid(data.email)){
+            return false
+        }
+
+        if(!isRequiredFieldValid(data.password)) {
+            return false
+        }
+        return true
+    }
+
+    function isRequiredFieldValid(value: any){
+        console.log(value != null && value !== "", "isRequiredFieldValid")
+        return value != null && value !== ""
+    }
+
+    // function onSubmit(e: SubmitEvent){
+    function onSubmit(e: SubmitEvent){
+        const formData = new FormData(e.target as HTMLFormElement);
+        const resetForm = e.target as HTMLFormElement;
+
+        console.log(formData)
+
+        const data: Record<string, FormDataEntryValue> = {};
+        for (let field of formData) {
+            const [key, value] = field;
+            data[key] = value;
+        }
+        if(isFormValid(data)){
+            console.log(data)
+
+            // e.target?.reset();
+            resetForm.reset()
+        } else {
+            console.log("Invalid form")
+        }
+    }
+
+
+</script>
+
+<main>
+    <form on:submit|preventDefault={onSubmit}>
+        <div>
+            <label for="name">Email</label>
+            <input
+            type="text"
+            id="email"
+            name="email"
+            value=""
+            />
+        </div>
+        <div>
+        <label for="name">Password</label>
+        <input
+            type="password"
+            id="password"
+            name="password"
+            value=""
+        />
+    </div>
+        <button type="submit">Submit</button>
+    </form>
+</main>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <style>
+     * {
+        box-sizing: border-box;
+      }
+      form {
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+      }
+
+      form > div{
+        display: flex;
+        justify-content: space-between;
+      }
+
+      form > div + * {
+        margin-top: 10px;
+      }
+
+      :global(html){
+        background-color: teal
+      }
+  </style>
